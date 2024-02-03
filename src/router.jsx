ace667092b9b12186eb2ef4be1eaa8dafc70daa3
Admin <skhurtov.work@yandex.ru>
2024-02-03 12:24:16 +0300

    feat: add router

---
+++ src/router.jsx
@@ -0,0 +1,43 @@
+import {
+    Navigate,
+    Outlet,
+    RouteObject,
+    RouterProvider,
+    createBrowserRouter,
+} from 'react-router-dom'
+import {Home} from './pages/home/home'
+import {LoginPage} from './pages/loginPage/LoginPage'
+
+
+
+const publicRoutes = [
+    {
+        element: <LoginPage />,
+        path: '/login',
+    },
+]
+
+const privateRoutes = [
+    {
+        element: <Home />,
+        path: '/',
+    },
+]
+
+const router = createBrowserRouter([
+    {
+        children: privateRoutes,
+        element: <PrivateRoutes />,
+    },
+    ...publicRoutes,
+])
+
+export const Router = () => {
+    return <RouterProvider router={router} />
+}
+
+function PrivateRoutes() {
+    const isAuthenticated = true
+
+    return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} />
+}
---
+++ src/services/base-api.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const baseApi = createApi({
+    reducerPath: 'baseApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://kinopoiskapiunofficial.tech',
+        credentials: 'include',
+        prepareHeaders: headers => {
+            headers.append('X-API-KEY', '311f6ed2-2da0-4a4e-bda9-3c31df66674a');
+        },
+    }),
+    endpoints: builder => {
+        return {
+            getFilms: builder.query({
+                query: () => `/api/v2.2/films/{id}`,
+            }),
+        }
+    },
+})
+
+export const { useGetFilmsQuery } = baseApi
