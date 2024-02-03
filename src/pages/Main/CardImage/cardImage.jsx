0e9d789d1b2eed0ed5bd030a8abf653a25a8cf2c
TatShen <shenets-97@mail.ru>
2024-02-03 16:14:14 +0300

    addc

--- src/pages/Main/CardImage/cardImage.jsx
+++ src/pages/Main/CardImage/cardImage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from './cardImage.module.scss'
 export default function CardImage({data}) {
   return (
-    <img src={data?.posterUrl} alt="Картинка" className={styles}/>
+    <img src={data?.posterUrl} alt="Картинка" className={styles.img}/>
   )
   
 }
