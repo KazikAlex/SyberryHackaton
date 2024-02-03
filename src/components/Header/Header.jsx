e9837fd4ca6c41e48ca61c39b4355807ca6bafb1
TatShen <shenets-97@mail.ru>
2024-02-03 13:08:59 +0300

    add state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,38 @@
-import React from "react"
-import styles from './Header.module.scss'
-import Container from "../Container/Container"
-import Button from '../Button/Button';
+import React from "react";
+import styles from "./Header.module.scss";
+import Container from "../Container/Container";
+import Button from "../Button/Button";
+import { useState } from "react";
+
+export default function Header() {
+  const [isUserLog, setUserLog] = useState(true);
+  
 
 
-export default function Header (){
   return (
     <Container className={styles.header}>
-      <header className={styles.header__container}>  
-        <div className={styles.logo}>
-          <img src='icons/logo.svg' alt="logo"></img>
+      <header className={styles.header__container}>
+        <div className={styles.logo} id="/">
+          <img src="icons/logo.svg" alt="logo"></img>
           <p className={styles.logo_p}>HackaFilm</p>
-        </div> 
-        <div className={styles.favorite}>
-            <img className={styles.favorite_img} src='icons/star-svgrepo-com.svg' alt="star"></img>
+        </div>
+        {isUserLog && (
+          <div className={styles.favorite}>
+            <img
+              className={styles.favorite_img}
+              src="icons/star-svgrepo-com.svg"
+              alt="star"
+            ></img>
             <p className={styles.favorite_p}>Favorites film</p>
-        </div> 
-          <form className={styles.form}>
-            <input className={styles.input} type="text" name="" id=""/> 
-            <Button context={"Поиск"} className={styles.header_btn}></Button> 
-          </form>
-        <Button context={"Войти"} className={styles.header_btn}></Button> 
-      </header> 
+          </div>
+        )}
+
+        <form className={styles.form}>
+          <input className={styles.input} type="text" name="" id="" />
+          <Button context={"Поиск"} className={styles.header_btn}></Button>
+        </form>
+        <Button context={"Войти"} className={styles.header_btn}></Button>
+      </header>
     </Container>
-  )
+  );
 }
