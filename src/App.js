9ba7ecf3f8822375618deda5a5b1a142917a2feb
Admin <skhurtov.work@yandex.ru>
2024-02-03 12:48:01 +0300

    feat: add rout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { Layout } from './components/layout/layout'
 import { Router } from './router'
 import { store } from './services/store'
+
 export const App = () => {
     return (
         <Provider store={store}>
