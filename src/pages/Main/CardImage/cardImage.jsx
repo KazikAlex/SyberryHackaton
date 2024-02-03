e3d21da66d3aee1e66b9cf3163ff1bfbb8a757ab
ajiltOk <21summer12@gmail.com>
2024-02-03 13:59:48 +0300

    feature: add data to main

--- src/pages/Main/CardImage/cardImage.jsx
+++ src/pages/Main/CardImage/cardImage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Img from "./img/about-pets-320.png";
 
 export default function CardImage() {
-  return <img src={Img} alt="Картинка" />;
+  //const {data} = use()
+  return <img src={data.posterUrl} alt="Картинка" />;
 }
