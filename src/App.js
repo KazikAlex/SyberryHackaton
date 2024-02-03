46418388cfaf85651c4f1b7949d53ba95881b955
Alexandr Kazimirov <akazimirov@tut.by>
2024-02-03 11:23:03 +0300

    recreate app.css and index.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>Hello Syberry</header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
