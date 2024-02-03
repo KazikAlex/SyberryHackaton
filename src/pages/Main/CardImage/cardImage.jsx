2665c2471edc30f30acc99e51cd8d336f89469b5
TatShen <shenets-97@mail.ru>
2024-02-03 16:55:28 +0300

    asa

--- src/pages/Main/CardImage/cardImage.jsx
+++ src/pages/Main/CardImage/cardImage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import styles from './cardImage.module.scss'
 
-export default function CardImage(data) {
-  //const {data} = use()
-  return null;
-  // console.log(data)
-  // return <img src={data.items.posterUrlPreview} alt="Картинка" />;
-}
+export default function CardImage({data}) {
+  return (
+    <img src={data?.posterUrl} alt="Картинка" className={styles.img}/>
+  )
+  }
